bffad4bf2b5afc4332f8af095d915220e2b97fb5
Ivan Panifedov <ivan.panifedov@gmail.com>
2023-04-19 04:37:47 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -150,19 +150,7 @@ import {
         "Participating in code reviews and providing constructive feedback to other developers.",
       ],
     },
-    // {
-    //   title: "Full stack Developer",
-    //   company_name: "Meta",
-    //   icon: meta,
-    //   iconBg: "#E6DEDD",
-    //   date: "Jan 2023 - Present",
-    //   points: [
-    //     "Developing and maintaining web applications using React.js and other related technologies.",
-    //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-    //     "Implementing responsive design and ensuring cross-browser compatibility.",
-    //     "Participating in code reviews and providing constructive feedback to other developers.",
-    //   ],
-    // },
+
   ];
   
   const testimonials = [
@@ -170,24 +158,21 @@ import {
       testimonial:
         "Ivan knows his business well and always tries to learn better.",
       name: "Sara Lee",
-      // designation: "CFO",
-      // company: "Acme Co",
+
       image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
       testimonial:
         "Ivan is a very diligent and hardworking guy.",
       name: "Chris Brown",
-      // designation: "COO",
-      // company: "DEF Corp",
+
       image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       testimonial:
         "Ivan, well done, keep up the good work! I'd love to see your next projects!",
       name: "Lisa Wang",
-      // designation: "CTO",
-      // company: "456 Enterprises",
+
       image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
   ];
@@ -202,60 +187,12 @@ import {
           name: "react",
           color: "blue-text-gradient",
         },
-        // {
-        //   name: "mongodb",
-        //   color: "green-text-gradient",
-        // },
-        // {
-        //   name: "tailwind",
-        //   color: "pink-text-gradient",
-        // },
+
       ],
       image: carrent,
       source_code_link: "https://github.com/panifedov",
     },
-    // {
-    //   name: "Job IT",
-    //   description:
-    //     "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    //   tags: [
-    //     {
-    //       name: "react",
-    //       color: "blue-text-gradient",
-    //     },
-    //     {
-    //       name: "restapi",
-    //       color: "green-text-gradient",
-    //     },
-    //     {
-    //       name: "scss",
-    //       color: "pink-text-gradient",
-    //     },
-    //   ],
-    //   image: jobit,
-    //   source_code_link: "https://github.com/panifedov",
-    // },
-    // {
-    //   name: "Trip Guide",
-    //   description:
-    //     "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    //   tags: [
-    //     {
-    //       name: "nextjs",
-    //       color: "blue-text-gradient",
-    //     },
-    //     {
-    //       name: "supabase",
-    //       color: "green-text-gradient",
-    //     },
-    //     {
-    //       name: "css",
-    //       color: "pink-text-gradient",
-    //     },
-    //   ],
-    //   image: tripguide,
-    //   source_code_link: "https://github.com/panifedov",
-    // },
+
   ];
   
   export { services, technologies, experiences, testimonials, projects };
